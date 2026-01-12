510d767267e779a4fd9114496fd0c30c999af538
vigneshkavivk <vigneshkavivk@gmail.com>
2026-01-12 11:08:32 +0530

    Delete app-cloudmasa directory

--- app-cloudmasa/server/config/socketConfig.js
+++
@@ -1,7 +0,0 @@
-// server/config/socketConfig.js
-export const socketOptions = {
-  cors: {
-    origin: "*",
-    methods: ["GET", "POST"]
-  }
-};
--- app-cloudmasa/server/routes/socketRoutes.js
+++
@@ -1,6 +0,0 @@
-// server/routes/socketRoutes.js
-import { setupSocketHandlers } from '../services/socketService.js';
-
-export default (io) => {
-  setupSocketHandlers(io);
-};
